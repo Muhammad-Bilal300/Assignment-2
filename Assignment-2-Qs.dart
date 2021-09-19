341e300131acbb2206b07031469b021de85af9d7
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2021-09-19 23:55:04 +0500

    Assignment-2 Completed

--- Assignment-2-Qs.dart
+++ Assignment-2-Qs.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
 // Q1) What are the various types of operators in dart? Explain with Examples.
 
@@ -22,6 +24,32 @@ void main() {
   print(--b);
   print(b--);
 
+
+  // Assignment Operator
+
+  a = 10;
+  
+
+  print(a+=3);
+  print(a-=3);
+  print(a*=3);
+  print(a~/=3);
+  print(a%=3);
+  
+  // Relational Operator
+
+print(10>2);
+print(10<2);
+print(10>=2);
+print(10<=2);
+print(10!=2);
+print(10==2);
+
+
+
+
+
+
 // Q2) What will be the output in variables a, b & result after execution of the
 //     following script:
 
@@ -40,6 +68,11 @@ void main() {
   var result = --a - --b + ++b + b--;
   print(result);
 
+
+
+
+
+
 // Q3) Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a
 //     variable & calculate the cost of buying 5 tickets to a movie.
 
@@ -49,6 +82,11 @@ void main() {
   num numberOfTicket = 5;
 
   print("Total cost of ${numberOfTicket} is: ${eachTicket}");
+
+
+
+
+
 
 //  Q4) How to get difference of lists in Dart?
 //    Problem: Consider you have two lists [1,2,3,4,5,6,7] and [3,5,6,7,9,10]. How
@@ -60,6 +98,12 @@ void main() {
   var lst2 = [3, 5, 6, 7, 9, 10];
 
   print(lst1.where((e) => !lst2.contains(e)).toList());
+
+
+
+
+
+
 
 // Q5) What is a difference between these operators “?? And?”
 
@@ -82,6 +126,12 @@ void main() {
   result = _val ?? val2;
   print(result);
 
+
+
+
+
+
+
 // Q6) What are the data types supported in Dart? Explain with Examples.
 
 // Answer:
@@ -100,4 +150,151 @@ void main() {
 
 //    List and Map :
 //       The data types list and map are used to represent a collection of objects. A List is an ordered group of objects. The List data type in Dart is synonymous to the concept of an array in other programming languages. The Map data type represents a set of values as key-value pairs. The dart: core library enables creation and manipulation of these collections through the predefined List and Map classes respectively.
+
+
+
+
+
+
+// Q7) Solve:
+  // a. First declare an array and assign the numbers of the table of 7.
+  // b. Second declare another array and assign the numbers 1-10
+  // c. Now write down the table of 7 using map.fromiterables method.
+
+// Answer :
+
+  var values = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+  var lineNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var table = Map.fromIterables(lineNumbers, values);
+  print(table);
+
+
+
+
+
+
+
+
+//  Q8) Write a program that
+  // a. Store correct password in a JS variable.
+  // b. Asks user to enter his/her password
+  // c. Validate the two passwords:
+  // d. Check if user has entered password. If not, then give message “Please
+  // enter your password”
+  // e. Check if both passwords are same. If they are same, show message
+  // “Correct! The password you
+  // f. entered matches the original password”. Show “Incorrect password”
+  // otherwise.
+
+// Answer:
+
+  var correctPass = "abc12345";
+
+  var userPass = "xyz789";
+  if (userPass.isEmpty) {
+    print("Please enter your Password");
+  } else {
+    if (correctPass == userPass) {
+      print("Correct! The password you entered matches the original password");
+    } else {
+      print("Incorrect Password");
+    }
+  }
+
+
+
+
+
+
+
+
+//  Q9) Write a program to store 3 student names in an array. Take another array to
+  // store score of these three students. Assume that total marks are 500 for each
+  // student, display the scores & percentages of students.
+
+  // Answer :
+
+  var names = ["Talha", "Ali", "Bilal"];
+  var scores = [300, 400, 450];
+  var totalMarks = 500;
+  var percentage;
+  for (var i = 0; i < names.length; i++) {
+    percentage = (scores[i] / 500) * 100;
+    print(
+        "The Score of ${names[i]} is ${scores[i]} out of ${totalMarks}.Percantage is : ${percentage}");
+  }
+
+
+
+
+
+
+
+
+// Q10) Declare 5 legal & 5 illegal variable names.
+
+// Answer:
+
+// Legal Variables
+
+// 1) name
+// 2) name1
+// 3) _name
+// 4) name_1
+// 5) $name
+
+// Legal Variables
+
+// 1) 1name
+// 2) @name;
+// 3) name-1
+// 4) name@1
+// 5) First name
+
+
+
+
+
+
+
+
+// Q11) Write a program to replace the “Hyder” to “Islam” in the word
+  // “Hyderabad” and display the result.
+
+// Answer:
+
+  var str = "Hyderabad";
+  print(str);
+  var lst = str.split("");
+  lst.replaceRange(0, 5, ["I", "s", "l", "a", "m"]);
+  str = lst.join("");
+  print(str);
+
+
+
+
+
+// Q12) Write a program to generate your K-Electric bill 7. All the amounts should
+// be rounded off to 2 decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+// Where, Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge 
+
+var Customer_name = "Muhammad Bilal";
+var currentMonth = "September";
+num number_of_Units = 150;
+num Charges_per_unit = 16;
+num netAmount = number_of_Units*Charges_per_unit;
+num latePaymentCharge = 200;
+num GrossAmount = latePaymentCharge+netAmount;
+
+print("Dear ${Customer_name}, Your bill of month ${currentMonth} is ${netAmount}. If you cant pay on time then after late Payment of ${latePaymentCharge} it becomes ${GrossAmount}.");
+
 }
